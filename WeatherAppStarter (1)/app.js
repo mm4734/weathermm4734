3ab8534430437496edf12f7395138e2a0497b3ef
mm4734 <mm4734@columbia.edu>
2017-03-29 20:32:10 -0400

    Update app.js

--- WeatherAppStarter (1)/app.js
+++ WeatherAppStarter (1)/app.js
@@ -1,9 +1,38 @@
+var APPID = "c4453c52a17a494925f51be443b0c422";
 var temp;
 var loc;
 var icon;
 var humidity;
 var wind;
-var direction; 
+var direction;
+
+function updateByZip(zip){
+  var url = "http://api.openweathermap.org/data/2.5/weather?" +
+    "zip=" + zip +
+    "&APPID=" + APPID;
+  sendRequest(url);
+
+}
+
+function sendRequest(url){
+  var xmlthttp = new XMLHTTPRequest();
+  xmlhttp.onreadystatechange = function(){
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      var data = JSON.parse(xmlhttp.responseText);
+      var weather = {};
+      weather.icon = data.weather[0].id;
+      weather.humidity = data.main.humidity;
+      weather.wind = data.wind.speed;
+      weather.direction = data.wind.deg;
+      weather.loc = data.name;
+      weather.temp = data.main.temp;
+      update(weather);
+    }
+  };
+  xmlhttp.open("GET" , url, true);
+  xmlhttp.send();
+
+}
 
 function update(weather){
   wind.innerHTML = weather.wind;
@@ -22,14 +51,6 @@ window.onload = function () {
   wind = document.getElementById("wind");
   direction = document.getElementById("direction");
 
-  var weather = {};
-  weather.wind = 3.5;
-  weather.direction = "N";
-  weather.humidity = 35;
-  weather.loc = "Boston";
-  weather.temp = "45";
-  weather.icon = 200;
-
-  update(weather);
+  updateByZip(87110);
 
 }
